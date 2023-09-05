84a3638eb4a88e120d56b5d8c2d5a27dafcd7c6a
john@johndeguise.com <john@johndeguise.com>
2023-09-05 16:43:31 +0200

    tweaks to output directory of server build

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 const path = require('node:path'); 
-require('dotenv').config({ path: path.resolve(__dirname, './../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 const PORT = process.env.NODE_PORT || 3001;
 
