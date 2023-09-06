3db1315c229d4b7382e047bdc7c9e5e9323682be
john@johndeguise.com <john@johndeguise.com>
2023-09-06 11:59:07 +0200

    cleaning up link string building

--- client/src/components/ReviewType/ReviewType.js
+++ client/src/components/ReviewType/ReviewType.js
@@ -2,37 +2,20 @@ import { Link } from "react-router-dom";
 import * as Constants from "./../../constants";
 
 const ReviewVocab = (props) => {
+  const PRACTICE_LINK = `?${Constants.SETNAME_QUERY_PARAM}=${props.setName}&${Constants.REVIEWTYPE_QUERY_PARAM}=${Constants.VOCAB_CARD_REVIEWTYPE_PRACTICE_STR}`;
+  const TEST_LINK = `?${Constants.SETNAME_QUERY_PARAM}=${props.setName}&${Constants.REVIEWTYPE_QUERY_PARAM}=${Constants.VOCAB_CARD_REVIEWTYPE_TEST_STR}`;
+
   return (
     <div className="review-type-options">
       <Link
         className="review-type-item"
-        to={
-          Constants.REVIEW_ENDPOINT +
-          "?" +
-          Constants.SETNAME_QUERY_PARAM +
-          "=" +
-          props.setName +
-          "&" +
-          Constants.REVIEWTYPE_QUERY_PARAM +
-          "=" +
-          Constants.VOCAB_CARD_REVIEWTYPE_PRACTICE_STR
-        }
+        to={PRACTICE_LINK}
       >
         {Constants.VOCAB_CARD_REVIEWTYPE_PRACTICE_LABEL}
       </Link>
       <Link
         className="review-type-item"
-        to={
-          Constants.REVIEW_ENDPOINT +
-          "?" +
-          Constants.SETNAME_QUERY_PARAM +
-          "=" +
-          props.setName +
-          "&" +
-          Constants.REVIEWTYPE_QUERY_PARAM +
-          "=" +
-          Constants.VOCAB_CARD_REVIEWTYPE_TEST_STR
-        }
+        to={TEST_LINK}
       >
         {Constants.VOCAB_CARD_REVIEWTYPE_TEST_LABEL}
       </Link>
